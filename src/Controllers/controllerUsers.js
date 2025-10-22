89fc35117bc5c982fc7d6ec080714c3a69287635
joao <joaobrum108@gmail.com>
2025-10-22 20:27:41 -0300

    crud usuarios

--- src/Controllers/controllerUsers.js
+++ src/Controllers/controllerUsers.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const serviceUsers = require('../Services/serviceUsers');
 
 class controllerUsers {
@@ -10,6 +11,89 @@ class controllerUsers {
       res.status(500).json({ error: e.message }); 
     }
   }
+
+  async listUserId(req, res) {
+  try {
+    const { id } = req.params; 
+
+    const result = await serviceUsers.listarById(id);
+
+    if (!result) {
+      return res.status(404).json({
+        message: 'Usuário não encontrado'
+      });
+    }
+
+    return res.status(200).json(result);
+  } catch (error) {
+    console.error('Erro ao listar usuário por ID:', error.message);
+
+    return res.status(500).json({
+      message: 'Erro interno ao buscar usuário',
+      error: error.message
+    });
+  }
+}
+
+
+ async listUsuario(req, res) {
+  try {
+    const result = await serviceUsers.listar();
+
+    return res.status(200).json(result);
+  } catch (error) {
+    console.error('Erro ao listar usuários:', error.message);
+
+    return res.status(500).json({
+      message: 'Erro interno ao listar usuários',
+      error: error.message
+    });
+  }
+}
+
+ async updateUser(req, res) {
+  try {
+    const { id } = req.params;
+    const { name, email, password, role } = req.body;
+
+    const result = await serviceUsers.atualizarUser(id, name, email, password, role);
+
+    if (!result) {
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+    }
+
+    return res.status(200).json(result);
+  } catch (error) {
+    console.error('Erro ao atualizar usuário:', error.message);
+    return res.status(500).json({
+      message: 'Erro interno ao atualizar o usuário',
+      error: error.message
+    });
+  }
+
+
+}
+
+async deleteUser(req, res) {
+  try {
+    const { id } = req.params;
+
+    const result = await serviceUsers.deletarUser(id);
+
+    if (!result) {
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+    }
+
+    return res.status(200).json(result);
+  } catch (error) {
+    console.error('Erro ao excluir usuário:', error.message);
+    return res.status(500).json({
+      message: 'Erro interno ao excluir o usuário',
+      error: error.message
+    });
+  }
+}
+
 }
 
 module.exports = new controllerUsers();
--- src/router.js
+++ src/router.js
@@ -3,6 +3,13 @@ const router = express.Router();
 
 const controllerUsers = require('./Controllers/controllerUsers'); 
 
+// crud usuarios basicao completo
+// concluido na quarta feira 
 router.post('/register', controllerUsers.RegisterUser);
+router.get('/listUsers' , controllerUsers.listUsuario);
+router.get('/users/:id' , controllerUsers.listUserId);
+router.put('/users/:id', controllerUsers.updateUser);
+router.delete('/delete/:id' , controllerUsers.deleteUser)
+
 
 module.exports = router;
